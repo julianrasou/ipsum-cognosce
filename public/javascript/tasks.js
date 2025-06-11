e5111bdba49ab677db9d41734a11157f583c2dab
julianrasou <julianrasou03@gmail.com>
2025-06-11 13:54:47 +0200

    Refactorizadas llamadas a APIs para usar un método y evitar diplucación de código

--- public/javascript/tasks.js
+++ public/javascript/tasks.js
@@ -7,16 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("closeDialogBtn").addEventListener("click", () => {
         document.getElementById("taskDetailsDialog").close();
     });
-    document.getElementById("addCategory").addEventListener("click", addCategory);
-    document.getElementById("removeCategory").addEventListener("click", removeCategory);
+    document
+        .getElementById("addCategory")
+        .addEventListener("click", addCategory);
+    document
+        .getElementById("removeCategory")
+        .addEventListener("click", removeCategory);
     document.getElementById("addTask").addEventListener("click", showAddTask);
-    document.getElementById("addTaskDialogButton").addEventListener("click", addTask);
-    document.getElementById("closeTaskDialogBtn").addEventListener("click", () => {
-        document.getElementById("addTaskDialog").close();
-        document.querySelector("#taskTitle").value = "";
-        document.querySelector("#taskDescription").value = "";
-    });
-})
+    document
+        .getElementById("addTaskDialogButton")
+        .addEventListener("click", addTask);
+    document
+        .getElementById("closeTaskDialogBtn")
+        .addEventListener("click", () => {
+            document.getElementById("addTaskDialog").close();
+            document.querySelector("#taskTitle").value = "";
+            document.querySelector("#taskDescription").value = "";
+        });
+});
 
 async function loadPage() {
     await loadCategories();
@@ -25,25 +33,25 @@ async function loadPage() {
 }
 
 function loadCategories() {
-    return fetch('app/api/categories.php', {
-        method: 'GET',
-        credentials: 'include'
+    return fetch("app/api/categories.php", {
+        method: "GET",
+        credentials: "include",
     })
-    .then(response => response.json())
-    .then(data => {
-        categories = data;
-    })
+        .then((response) => response.json())
+        .then((data) => {
+            categories = data;
+        });
 }
 
 function loadTasks() {
-    return fetch('app/api/tasks.php', {
-        method: 'GET',
-        credentials: 'include'
+    return fetch("app/api/tasks.php", {
+        method: "GET",
+        credentials: "include",
     })
-    .then(response => response.json())
-    .then(data => {
-        tasks = data;
-    })
+        .then((response) => response.json())
+        .then((data) => {
+            tasks = data;
+        });
 }
 
 function updateView() {
@@ -56,25 +64,29 @@ function updateView() {
 
 function updateCategoriesView() {
     let categoriesDiv = document.querySelector("#categories");
-    categoriesDiv.innerHTML = ""; 
+    categoriesDiv.innerHTML = "";
 
     let categoryUl = document.createElement("ul");
     let noCategoryLi = document.createElement("li");
-    noCategoryLi.appendChild( document.createTextNode( "Sin categoría" ) );
-    noCategoryLi.addEventListener("click", () => { updateSelectedCategory( "Sin categoría" ) });
-    if( "Sin categoría" === selectedCategory ) {
+    noCategoryLi.appendChild(document.createTextNode("Sin categoría"));
+    noCategoryLi.addEventListener("click", () => {
+        updateSelectedCategory("Sin categoría");
+    });
+    if ("Sin categoría" === selectedCategory) {
         noCategoryLi.classList.add("selected-category");
-    }else {
+    } else {
         noCategoryLi.classList.add("unselected-category");
     }
     categoryUl.appendChild(noCategoryLi);
-    categories.forEach(category => {
+    categories.forEach((category) => {
         let categoryLi = document.createElement("li");
-        categoryLi.appendChild( document.createTextNode( category["name"] ) );
-        categoryLi.addEventListener("click", () => { updateSelectedCategory( category["name"] ) });
-        if( category["name"] === selectedCategory ) {
+        categoryLi.appendChild(document.createTextNode(category["name"]));
+        categoryLi.addEventListener("click", () => {
+            updateSelectedCategory(category["name"]);
+        });
+        if (category["name"] === selectedCategory) {
             categoryLi.classList.add("selected-category");
-        }else {
+        } else {
             categoryLi.classList.add("unselected-category");
         }
         categoryUl.appendChild(categoryLi);
@@ -82,7 +94,7 @@ function updateCategoriesView() {
     categoriesDiv.appendChild(categoryUl);
 }
 
-function updateSelectedCategory( category ) {
+function updateSelectedCategory(category) {
     selectedCategory = category;
     updateView();
 }
@@ -92,122 +104,138 @@ function updateTasksView() {
     let filteredTasksCompleted = [];
     let tasksDiv = document.querySelector("#tasks");
     tasksDiv.innerHTML = "";
-    
 
-    tasks.forEach(task => {
+    tasks.forEach((task) => {
         let taskCategory;
-        for( let i = 0; i < categories.length ; i++ ){
-            if( task["category_id"] === categories[i]["id"] ) {
+        for (let i = 0; i < categories.length; i++) {
+            if (task["category_id"] === categories[i]["id"]) {
                 taskCategory = categories[i]["name"];
                 break;
             }
         }
-        if( selectedCategory === "Sin categoría" && task["category_id"] == null) {
+        if (
+            selectedCategory === "Sin categoría" &&
+            task["category_id"] == null
+        ) {
             filteredTasks.push(task);
-        } else if(taskCategory === selectedCategory){
-            if( task["status"] == true ) {
+        } else if (taskCategory === selectedCategory) {
+            if (task["status"] == true) {
                 filteredTasksCompleted.push(task);
             } else {
                 filteredTasks.push(task);
             }
         }
-        
     });
 
-    filteredTasks.forEach(task => {
+    filteredTasks.forEach((task) => {
         let singleTaskDiv = document.createElement("div");
         let taskCheckbox = document.createElement("input");
         taskCheckbox.setAttribute("type", "checkbox");
-        if( task["status"] == true) {
+        if (task["status"] == true) {
             taskCheckbox.checked = true;
             singleTaskDiv.classList.add("completed");
         }
-        taskCheckbox.addEventListener("change", () => {toggleCompleted(task)});
+        taskCheckbox.addEventListener("change", () => {
+            toggleCompleted(task);
+        });
         singleTaskDiv.appendChild(taskCheckbox);
         let taskSpan = document.createElement("span");
-        taskSpan.appendChild( document.createTextNode(task["title"]) );
-        taskSpan.addEventListener("click", () => { showTaskDetails(task) })
-        singleTaskDiv.appendChild( taskSpan );
+        taskSpan.appendChild(document.createTextNode(task["title"]));
+        taskSpan.addEventListener("click", () => {
+            showTaskDetails(task);
+        });
+        singleTaskDiv.appendChild(taskSpan);
         let removeButton = document.createElement("button");
         removeButton.textContent = "x";
         removeButton.classList.add("removeButton");
-        removeButton.addEventListener( "click", () => { removeTask( task["id"] ) } );
+        removeButton.addEventListener("click", () => {
+            removeTask(task["id"]);
+        });
         singleTaskDiv.appendChild(removeButton);
         tasksDiv.appendChild(singleTaskDiv);
     });
 
-    if( filteredTasksCompleted.length != 0) {
+    if (filteredTasksCompleted.length != 0) {
         let completedTittle = document.createElement("h2");
-        completedTittle.appendChild( document.createTextNode( "Completadas:" ) );
+        completedTittle.appendChild(document.createTextNode("Completadas:"));
         completedTittle.classList.add("completedTitle");
         tasksDiv.appendChild(completedTittle);
 
-        filteredTasksCompleted.forEach(task => {
+        filteredTasksCompleted.forEach((task) => {
             let singleTaskDiv = document.createElement("div");
             let taskCheckbox = document.createElement("input");
             taskCheckbox.setAttribute("type", "checkbox");
-            if( task["status"] == true) {
+            if (task["status"] == true) {
                 taskCheckbox.checked = true;
                 singleTaskDiv.classList.add("completed");
             }
-            taskCheckbox.addEventListener("change", () => {toggleCompleted(task)});
+            taskCheckbox.addEventListener("change", () => {
+                toggleCompleted(task);
+            });
             singleTaskDiv.appendChild(taskCheckbox);
             let taskSpan = document.createElement("span");
-            taskSpan.appendChild( document.createTextNode(task["title"]) );
-            taskSpan.addEventListener("click", () => { showTaskDetails(task) })
-            singleTaskDiv.appendChild( taskSpan );
+            taskSpan.appendChild(document.createTextNode(task["title"]));
+            taskSpan.addEventListener("click", () => {
+                showTaskDetails(task);
+            });
+            singleTaskDiv.appendChild(taskSpan);
             let removeButton = document.createElement("button");
             removeButton.textContent = "x";
             removeButton.classList.add("removeButton");
-            removeButton.addEventListener( "click", () => { removeTask( task["id"] ) } );
+            removeButton.addEventListener("click", () => {
+                removeTask(task["id"]);
+            });
             singleTaskDiv.appendChild(removeButton);
             tasksDiv.appendChild(singleTaskDiv);
         });
     }
 }
 
+function apiAccess(apiURL, config, errorMessage, success) {
+    fetch(apiURL, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(config),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                success();
+            } else {
+                alert(errorMessage);
+            }
+        });
+}
+
 function toggleCompleted(task) {
     let status = task["status"];
-    if(status) {
-        fetch("app/api/toggleCompleted.php", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
+    if (status) {
+        apiAccess(
+            "app/api/toggleCompleted.php",
+            {
                 id: task["id"],
-                newStatus: 0
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                loadPage();
-            } else {
-                alert(data.error || "Unknown error");
-            }
-        });
-    }else {
-        fetch("app/api/toggleCompleted.php", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
+                newStatus: 0,
             },
-            body: JSON.stringify({
-                id: task["id"],
-                newStatus: 1
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
+            "Error: No se pudo realizar la operación.",
+            () => {
                 loadPage();
-            } else {
-                alert(data.error || "Unknown error");
             }
-        });
+        );
+    } else {
+        apiAccess(
+            "app/api/toggleCompleted.php",
+            {
+                id: task["id"],
+                newStatus: 1,
+            },
+            "Error: No se pudo realizar la operación.",
+            () => {
+                loadPage();
+            }
+        );
     }
 }
 
@@ -216,11 +244,14 @@ function showTaskDetails(task) {
 
     document.getElementById("dialogTitle").textContent = task["title"];
 
-    document.getElementById("dialogDescription").textContent = task["description"] || "No description";
+    document.getElementById("dialogDescription").textContent =
+        task["description"] || "No description";
 
     document.getElementById("dialogCategory").textContent = selectedCategory;
 
-    document.getElementById("dialogStatus").textContent = task["status"] ? "Completada" : "Pendiente";
+    document.getElementById("dialogStatus").textContent = task["status"]
+        ? "Completada"
+        : "Pendiente";
 
     document.getElementById("dialogDate").textContent = task["created_at"];
 
@@ -230,54 +261,41 @@ function showTaskDetails(task) {
 function addCategory() {
     let categoryName = prompt("Indica el nombre de la nueva categoría:");
 
-    fetch("app/api/addCategory.php", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json"
+    apiAccess(
+        "app/api/addCategory.php",
+        {
+            name: categoryName,
         },
-        body: JSON.stringify({
-            name: categoryName
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
+        "Error: No se pudo realizar la operación.",
+        () => {
             loadPage();
-        } else {
-            alert(data.error || "Unknown error");
         }
-    });
+    );
 }
 
 function removeCategory() {
-    if(selectedCategory === "Sin categoría") {
+    if (selectedCategory === "Sin categoría") {
         alert("Seleccione una categoría");
         return;
     }
-    let confirmar = confirm("Desea eliminar la categoría " + selectedCategory + " y todas las tareas que contiene?");
+    let confirmar = confirm(
+        "Desea eliminar la categoría " +
+            selectedCategory +
+            " y todas las tareas que contiene?"
+    );
     if (confirmar) {
-        fetch("app/api/removeCategory.php", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
+        apiAccess(
+            "app/api/removeCategory.php",
+            {
+                name: selectedCategory,
             },
-            body: JSON.stringify({
-                name: selectedCategory
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
+            "Error: No se pudo realizar la operación.",
+            () => {
                 selectedCategory = "Sin categoría";
                 loadPage();
-            } else {
-                alert(data.error || "Unknown error");
             }
-        }); 
+        );
     }
-
 }
 
 function showAddTask() {
@@ -291,63 +309,47 @@ function addTask() {
     let description = document.querySelector("#taskDescription").value;
     document.querySelector("#taskDescription").value = "";
     let taskCategory = 0;
-    for( let i = 0; i < categories.length ; i++ ){
-        if( selectedCategory === categories[i]["name"] ) {
+    for (let i = 0; i < categories.length; i++) {
+        if (selectedCategory === categories[i]["name"]) {
             taskCategory = categories[i]["id"];
             break;
         }
     }
 
-    if(title.trim() === "") {
+    if (title.trim() === "") {
         alert("El título es obligatorio.");
-    }else {
-        fetch("app/api/addTask.php", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
+    } else {
+        apiAccess(
+            "app/api/addTask.php",
+            {
                 title: title,
                 description: description,
-                category: taskCategory
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
+                category: taskCategory,
+            },
+            "Error: No se pudo realizar la operación.",
+            () => {
                 document.getElementById("addTaskDialog").close();
                 loadPage();
-            } else {
-                alert(data.error || "Unknown error");
             }
-        });
+        );
     }
-
 }
 
-function removeTask( taskId ) {
+function removeTask(taskId) {
     let confirmar = confirm("¿Desea eliminar la tarea definitivamente?");
 
-    if(!confirmar) {
+    if (!confirmar) {
         return;
     }
-    fetch("app/api/removeTask.php", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json"
+
+    apiAccess(
+        "app/api/removeTask.php",
+        {
+            taskId: taskId,
         },
-        body: JSON.stringify({
-            taskId: taskId
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
+        "Error: No se pudo realizar la operación.",
+        () => {
             loadPage();
-        } else {
-            alert(data.error || "Unknown error");
         }
-    });
+    );
 }
