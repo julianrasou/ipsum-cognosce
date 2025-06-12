1034c32b882fa3bb1ced0cc122268b20c81978bd
julianrasou <julianrasou03@gmail.com>
2025-06-12 12:53:15 +0200

    Añadida view para pomodoro y estilos

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -3,5 +3,33 @@
 -->
 
 <main>
-   <h1>fsdf</h1> 
+
+    <div class="pomodoro-container">
+        <div class="timer">
+            <h1>Pomodoro timer</h1>
+            <p class="cicles">Pomodoros completados: <strong><span id="cicles"></span></strong></p>
+            <div class="button-container">
+                <button class="pomodoro-button" id="pomodoro-session">Pomodoro</button>
+                <button class="pomodoro-button" id="short-break">Descanso</button>
+            </div>
+
+            <section class="pomodoro">
+                <div id="pomodoro-timer" class="timer-display" data-duration="25.00">
+                    <h1 class="time">25:00</h1>
+                </div>
+                <div id="short-timer" class="timer-display" data-duration="5.00">
+                    <h1 class="time">5:00</h1>
+                </div>
+            </section>
+
+            <div class="buttons">
+                <button class="control-button" id="pomodoro-start">Start</button>
+                <button class="control-button" id="pomodoro-reset">Reset</button>
+                <button class="control-button" id="pomodoro-stop">Stop</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Script JavaScript -->
+     <script src="public/javascript/pomodoro.js"></script>
 </main>
